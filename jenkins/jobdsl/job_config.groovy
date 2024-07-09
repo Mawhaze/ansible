1e05b8b8830ade3dd25e73caa0e46638f0287124
mawhaze <evancast92@gmail.com>
2024-07-09 12:33:12 -0600

    more testing

--- jenkins/jobdsl/job_config.groovy
+++ jenkins/jobdsl/job_config.groovy
@@ -5,45 +5,44 @@ folder('ansible/playbooks') {
 
 // Ansible job definitions
 // Define the ubuntu_os_updates pipeline job within the ansible folder
-  pipelineJob('ansible/playbooks/ubuntu_os_updates') {
-    definition {
-      cps {
-        // Inline Groovy script for pipeline definition
-        script("""
+pipelineJob('ansible/playbooks/ubuntu_os_updates') {
+  definition {
+    cps {
+      // Inline Groovy script for pipeline definition
+      script("""
 pipeline {
-    agent any
-    stages {
-        stage('Sign into DockerHub and Pull Docker Image') {
-            steps {
-                script {
-                    docker.withRegistry('https://index.docker.io/v1/', 'dockerhub_credentials') {
-                        // Pull the Docker image from DockerHub before running it
-                        sh "docker pull mawhaze/ansible:latest"
-                    }
-                }
-            }
-        }
-        stage('Run Ansible Playbook') {
-            steps {
-                withCredentials([
-                    string(credentialsId: 'sa_ansible_aws_access_key_id', variable: 'AWS_ACCESS_KEY_ID'),
-                    string(credentialsId: 'sa_ansible_aws_secret_access_key', variable: 'AWS_SECRET_ACCESS_KEY')
-                ]) {
-                    script {
-                        // Run the Ansible playbook
-                        sh "docker run -e AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} -e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} \
-                            mawhaze/ansible:latest \
-                            ansible-playbook -i /etc/ansible/inventories/inventory.proxmox.yml /etc/ansible/playbooks/updates/os_updates.yml"
-                    }
-                }
-            }
-        }
-    }
-}
-        """)
+  agent any
+  stages {
+      stage('Sign into DockerHub and Pull Docker Image') {
+          steps {
+              script {
+                  docker.withRegistry('https://index.docker.io/v1/', 'dockerhub_credentials') {
+                      // Pull the Docker image from DockerHub before running it
+                      sh "docker pull mawhaze/ansible:latest"
+                  }
+              }
+          }
       }
+      stage('Run Ansible Playbook') {
+          steps {
+              withCredentials([
+                  string(credentialsId: 'sa_ansible_aws_access_key_id', variable: 'AWS_ACCESS_KEY_ID'),
+                  string(credentialsId: 'sa_ansible_aws_secret_access_key', variable: 'AWS_SECRET_ACCESS_KEY')
+              ]) {
+                  sh(
+                      'docker run -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+                      mawhaze/ansible:latest \
+                      ansible-playbook -i /etc/ansible/inventories/inventory.proxmox.yml /etc/ansible/playbooks/updates/os_updates.yml'
+                  )
+              }
+          }
+      }
+  }
+}
+      """)
     }
   }
+}
 
 // Docker jobs for Ansible definitions
 // Define the ansible_docker build job within the docker/build folder
