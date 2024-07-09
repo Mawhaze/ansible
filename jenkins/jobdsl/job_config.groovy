74734bae050005332e872c2059b17f8efa4ba4e8
mawhaze <evancast92@gmail.com>
2024-07-09 12:26:11 -0600

    testing aws injection

--- jenkins/jobdsl/job_config.groovy
+++ jenkins/jobdsl/job_config.groovy
@@ -31,7 +31,7 @@ pipeline {
                 ]) {
                     script {
                         // Run the Ansible playbook
-                        sh "docker run -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+                        sh "docker run -e AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} -e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} \
                             mawhaze/ansible:latest \
                             ansible-playbook -i /etc/ansible/inventories/inventory.proxmox.yml /etc/ansible/playbooks/updates/os_updates.yml"
                     }
