26edb0bbe68a58db06c4ab1720e1e6d624b4ed6a
mawhaze <evancast92@gmail.com>
2024-07-09 15:19:31 -0600

    testing update play

--- jenkins/jobdsl/job_config.groovy
+++ jenkins/jobdsl/job_config.groovy
@@ -5,6 +5,46 @@ folder('ansible/playbooks') {
 
 // Ansible job definitions
 // Keep this alphabetical for easier maintenance
+// Define the ubuntu_docker_container_updates
+pipelineJob('ansible/playbooks/ubuntu_docker_container_updates') {
+  definition {
+    cps {
+      // Inline Groovy script for pipeline definition
+      script("""
+pipeline {
+  agent any
+  stages {
+      stage('Sign into DockerHub and Pull Docker Image') {
+          steps {
+              script {
+                  docker.withRegistry('https://index.docker.io/v1/', 'dockerhub_credentials') {
+                      // Pull the Docker image from DockerHub before running it
+                      sh "docker pull mawhaze/ansible:latest"
+                  }
+              }
+          }
+      }
+      stage('Run Ansible Playbook') {
+          steps {
+              withCredentials([
+                  string(credentialsId: 'sa_ansible_aws_access_key_id', variable: 'AWS_ACCESS_KEY_ID'),
+                  string(credentialsId: 'sa_ansible_aws_secret_access_key', variable: 'AWS_SECRET_ACCESS_KEY')
+              ]) {
+                  sh(
+                      'docker run -e AWS_ACCESS_KEY_ID=\$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=\$AWS_SECRET_ACCESS_KEY \
+                      mawhaze/ansible:latest \
+                      ansible-playbook -i /etc/ansible/inventories/inventory.proxmox.yml /etc/ansible/playbooks/ubuntu/docker_updates.yml'
+                  )
+              }
+          }
+      }
+  }
+}
+      """)
+    }
+  }
+}
+
 // Define the ubuntu_os_updates
 pipelineJob('ansible/playbooks/ubuntu_os_updates') {
   definition {
