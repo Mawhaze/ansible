e825b7d5aa8f4c5352467acab0b72214b0c20c44
mawhaze <evancast92@gmail.com>
2024-09-05 10:51:34 -0600

    initial playbook test for k8s config

--- jenkins/jobdsl/job_config.groovy
+++ jenkins/jobdsl/job_config.groovy
@@ -134,6 +134,44 @@ pipeline {
   }
 }
 
+// Define the ubuntu_deploy_k8s_cluster job
+pipelineJob('ansible/playbooks/ubuntu_deploy_k8s_cluster') {
+  logRotator {
+    numToKeep(10) //Only keep the last 10
+  }
+  definition {
+    cps {
+      // Inline Groovy script for pipeline definition
+      script("""
+pipeline {
+  agent any
+  stages {
+    stage('Sign into DockerHub and Pull Docker Image') {
+        steps {
+            script {
+                docker.withRegistry('https://index.docker.io/v1/', 'dockerhub_credentials') {
+                    // Pull the Docker image from DockerHub before running it
+                    sh "docker pull mawhaze/ansible:latest"
+                }
+            }
+        }
+    }
+    stage('Run Ansible Playbook') {
+        steps {
+            withCredentials([
+                string(credentialsId: 'sa_ansible_aws_access_key_id', variable: 'AWS_ACCESS_KEY_ID'),
+                string(credentialsId: 'sa_ansible_aws_secret_access_key', variable: 'AWS_SECRET_ACCESS_KEY')
+            ]) {
+              sh(
+                  'docker run -e AWS_ACCESS_KEY_ID=\$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=\$AWS_SECRET_ACCESS_KEY \
+                  mawhaze/ansible:latest \
+                  ansible-playbook -i /etc/ansible/inventories/inventory.proxmox.yml /etc/ansible/playbooks/ubuntu/test_k8s.yml'
+              )
+      """)
+    }
+  }
+}
+
 // Docker build job for Ansible
 // Define the ansible_docker build job within the docker/build folder
 pipelineJob('docker/build/ansible_docker') {
